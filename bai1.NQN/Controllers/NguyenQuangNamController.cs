9019c191b2e84431117793367b927bcf1f0e87c9
nam31804 <nam31804@gmail.com>
2024-04-17 22:55:11 +0700

    Add project files.

---
+++ bai1.NQN/Controllers/NguyenQuangNamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bai1.NQN.Controllers
+{
+    public class NguyenQuangNamController : Controller
+    {
+        // GET: NguyenQuangNam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ bai1.NQN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bai1.NQN.MvcApplication" Language="C#" %>
